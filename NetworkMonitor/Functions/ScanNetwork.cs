7cf5f4ef715c2d84a9c0bccf60c3c5c04d2f2413
looZ149 <nklitsch@web.de>
2025-04-18 14:21:42 +0200

    fixing code

--- NetworkMonitor/Functions/ScanNetwork.cs
+++ NetworkMonitor/Functions/ScanNetwork.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 
 
 
@@ -28,40 +29,63 @@ namespace NetworkMonitor.Functions
             }
             throw new Exception("Couldnt find local ip"); // In case we cant find the local ip, throw exception with a msg, still continue
         }
-
-        public ObservableCollection<NetworkDevices> ScanLocalNetwork(string baseip)
+            
+        //Quantum code error
+        //Run code with a breakpoint -> code works - Sometimes throws an error tho?
+        //Run code without a breakpoint -> code doesnt work??
+        
+        public async Task<ObservableCollection<NetworkDevices>> ScanLocalNetwork(string baseip)
         {
-            ObservableCollection<NetworkDevices> devicesOnline = new();
-            var tasks = new List<Task>(); // Create a list of Tasks to ping multiple IP's at once
             using Ping ping = new Ping();
+            ObservableCollection<NetworkDevices> devicesOnline = new();
 
-            for (int i = 0; i < 255; i++)
+            for(int i = 0; i < 255; i++)
             {
                 string ip = $"{baseip}.{i}";
-                tasks.Add(Task.Run(async () =>
+                var reply = await ping.SendPingAsync(ip, 100);
+                if(reply.Status == IPStatus.Success)
                 {
-                    try
-                    {
-
-                        var reply = await ping.SendPingAsync(ip, 100);
-                        if (reply.Status == IPStatus.Success)
-                        {
-                            //Lock the list so we ensure only one Task adds his result to it.
-                            lock (devicesOnline)
-                            {
-                                devicesOnline.Add(new NetworkDevices { IPAddress = ip, IsOnline = true });
-                            }
-
-                        }
-                    }
-                    catch
-                    {
-                        //can pretty much just let any error pass ?
-                    }
-                }));
+                    devicesOnline.Add(new NetworkDevices { IPAddress = ip, IsOnline = true });
+                }
             }
+            
             return devicesOnline;
         }
+
+        //public async Task<ObservableCollection<NetworkDevices>> ScanLocalNetwork(string baseip)
+        //{
+        //    ObservableCollection<NetworkDevices> devicesOnline = new();
+        //    var tasks = new List<Task>(); // Create a list of Tasks to ping multiple IP's at once
+        //    using Ping ping = new Ping(); // Apparently not thread-safe, while its used by multiple tasks -> could produce the "Quantum code" error here
+
+        //    for (int i = 0; i < 255; i++)
+        //    {
+        //        string ip = $"{baseip}.{i}";
+        //        tasks.Add(Task.Run(async () =>
+        //        {
+        //            try
+        //            {
+
+        //                var reply = await ping.SendPingAsync(ip, 100);
+        //                if (reply.Status == IPStatus.Success)
+        //                {
+        //                    //Lock the list so we ensure only one Task adds his result to it.
+        //                    lock (devicesOnline)
+        //                    {
+        //                        devicesOnline.Add(new NetworkDevices { IPAddress = ip, IsOnline = true });
+        //                    }
+
+        //                }
+        //            }
+        //            catch
+        //            {
+        //                //can pretty much just let any error pass ?
+        //            }
+        //        }));
+        //    }
+        //    await Task.WhenAll(tasks);
+        //    return devicesOnline;
+        //}
 
         //Function gets called on Launch - Gets all active devices and populates the list with ips
         public void GetDevices()
